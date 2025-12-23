edd2dde31fc8c0218c523fc490c6067bd1f86429
Vipin Dev S <vipindevs@Vipins-MacBook-Air.local>
2025-12-23 17:53:48 +0530

    Fix firebase

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  output: 'export',
+  images: { unoptimized: true }, // required for next/image on static export
 };
 
 export default nextConfig;
