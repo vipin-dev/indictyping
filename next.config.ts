10a37555f53def1ddf088427a873d7c693ec79e9
Vipin Dev S <vipindevs@Vipins-MacBook-Air.local>
2025-12-26 10:47:51 +0530

    Remove static site

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  output: 'export',
-  images: { unoptimized: true }, // required for next/image on static export
 };
 
 export default nextConfig;
